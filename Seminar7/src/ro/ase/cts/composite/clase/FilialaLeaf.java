b208e95c9b35119cd704d0c8c45a5cac1afd81b0
Jardianu <52535108+Jardianu@users.noreply.github.com>
2021-04-15 17:16:56 +0300

    Composite, Facade, Proxy

---
+++ Seminar7/src/ro/ase/cts/composite/clase/FilialaLeaf.java
@@ -0,0 +1,30 @@
+package ro.ase.cts.composite.clase;
+
+
+public class FilialaLeaf implements IfSediu{
+    private String numeFiliala;
+
+    public FilialaLeaf(String numeFiliala) {
+        this.numeFiliala = numeFiliala;
+    }
+
+    @Override
+    public void afiseazaDescriere() {
+        System.out.println("Este o filiala - " + this.numeFiliala);
+    }
+
+    @Override
+    public void adaugaNod(IfSediu sediu) throws Exception {
+        throw new Exception("Nu este implementat");
+    }
+
+    @Override
+    public void stergeNod(IfSediu sediu) throws Exception{
+        throw new Exception("Nu este implementat");
+    }
+
+    @Override
+    public IfSediu getNod(int poz) throws Exception {
+        throw new Exception("Nu este implementat");
+    }
+}
