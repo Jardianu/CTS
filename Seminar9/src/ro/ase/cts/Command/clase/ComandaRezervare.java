4d92419516f636bf1ab9706e5a87e175b711618d
Jardianu <52535108+Jardianu@users.noreply.github.com>
2021-04-22 19:17:55 +0300

    Command

---
+++ Seminar9/src/ro/ase/cts/Command/clase/ComandaRezervare.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.Command.clase;
+
+public class ComandaRezervare extends Comanda{
+    public ComandaRezervare(Masa masa) {
+        super(masa);
+    }
+
+    @Override
+    public void executa() {
+        super.masa.rezervaMasa();
+    }
+}
---
+++ Seminar9/src/ro/ase/cts/Command/program/main.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.Command.program;
+
+import ro.ase.cts.Command.clase.*;
+public class main {
+
+	public static void main(String[] args) {
+		Masa masa1 = new Masa(1);
+        Masa masa2 = new Masa(2);
+        Comanda comandaRezervare = new ComandaRezervare(masa1);
+        Comanda comandaOcupare = new ComandaOcupare(masa2);
+        Comanda comandaOcupare2 = new ComandaOcupare(masa2);
+
+        Operator operator = new Operator();
+        operator.invoca(comandaRezervare);
+        operator.executaComanda();
+        operator.invoca(comandaOcupare);
+        operator.invoca(comandaOcupare2);
+
+        operator.executaComanda();
+        operator.executaComanda();
+
+	}
+
+}
